f50055fa67b22295b92b1ae6af50e7b5c97456ca
RyanMacc <arbader2142@gmail.com>
2022-04-27 18:47:22 -0400

    mongodb connected through MONGODB_URI in env file

---
+++ config/db.connection.js
@@ -0,0 +1,20 @@
+// require mongoose to get connection to mongodb
+const mongoose = require('mongoose');
+//getting access to .env
+require('dotenv').config();
+// connection str variable (URI is from .env file with app link)
+const connectionStr = process.env.MONGODB_URI;
+
+mongoose.connect(connectionStr);
+
+mongoose.connection.on('connected', () => {
+    console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected... 🥳 🥳 🥳 `)
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('MongoDB connection error ', error);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected 🫠 🫠 🫠 ');
+});
