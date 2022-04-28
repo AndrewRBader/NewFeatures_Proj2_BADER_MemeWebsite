dc50fff85e0b5ca03d7ee0a8fd01b7afad740dee
RyanMacc <arbader2142@gmail.com>
2022-04-28 12:11:47 -0400

    just reordered stuff in server.js, everything still works

--- server.js
+++ server.js
@@ -26,10 +26,13 @@ app.use(express.static('public'));
 app.use(methodOverride('_method'));
 // body parser middleware
 app.use(express.urlencoded({extended:false}));
-// meme controller middleware
-app.use('/meme/', memeController);
 //application view engine to render ejs
 app.set('view engine', 'ejs');
+
+/////////////////////// CONTROLLERS ////////////////////////
+
+// meme controller middleware
+app.use('/meme/', memeController);
 
 /////////////////////// ROUTES //////////////////////////
 
